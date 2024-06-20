ec990b59505da0be4ec96e8a2d0719ad50d2d334
Dragon-Baroque <74261498+Dragon-Baroque@users.noreply.github.com>
2024-06-20 16:14:31 +0200

    Migrate mapedit from GTK+ 3 to GTK 4 - GTK 4 : Fixes on top
      macosx/exult_studio.bundle
        Dependency GTK+ 3 -> GTK 4
      mapedit/studio.cc
        One blank line
      mapedit/combo.cc + locator.cc + objbrowse.cc :
        Do not remove the EventControllers ( crash closing Group window )
      mapedit/exult_studio.glade
        Initialize Combo selection,
        Remove gtk-ok + cancel + apply, Rework gtk-edit + close ( Broken icons in Adwaita )
        Complete GtkButtonBox to GtkBox using hexpand on Box and on children
      mapedit/shapedraw.cc
        Ensure the Shape is separate from the Pointer when dragged, discard its Origin, for macOS
      mapedit/objedit.cc
        Failed attempt to bring the Object Window on top of Exult on macOS
      mapedit/chunklst.cc + chunklst.h
        Use the browser sized chuck as the Drag icon

--- mapedit/objbrowse.cc
+++ mapedit/objbrowse.cc
@@ -42,20 +42,20 @@ Object_browser::~Object_browser() {
 	popup_widget = nullptr;
 	if ((G_IS_OBJECT(vscroll_ctlr))
 		&& (G_OBJECT(vscroll_ctlr)->ref_count > 0)) {
-		g_object_unref(vscroll_ctlr);
+		//		g_object_unref(vscroll_ctlr);
 	}
 	vscroll_ctlr = nullptr;
 #if GTK_CHECK_VERSION(4, 0, 0)    // GTK 4
 	if ((G_IS_OBJECT(key_ctlr)) && (G_OBJECT(key_ctlr)->ref_count > 0)) {
-		g_object_unref(key_ctlr);
+		//		g_object_unref(key_ctlr);
 	}
 	key_ctlr = nullptr;
 	if ((G_IS_OBJECT(click_ctlr)) && (G_OBJECT(click_ctlr)->ref_count > 0)) {
-		g_object_unref(click_ctlr);
+		//		g_object_unref(click_ctlr);
 	}
 	click_ctlr = nullptr;
 	if ((G_IS_OBJECT(drag_source)) && (G_OBJECT(drag_source)->ref_count > 0)) {
-		g_object_unref(drag_source);
+		//		g_object_unref(drag_source);
 	}
 	drag_source = nullptr;
 #else     // GTK 4
